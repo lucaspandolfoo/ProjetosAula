cc3f8a7097da7ae81a56707e24830411bc8ec829
Windows <Windows@Windows-PC>
2018-07-29 13:15:43 -0300

    Inserido validações no limite e saldo

--- src/Aula/Exercicio1.java
+++ src/Aula/Exercicio1.java
@@ -19,14 +19,14 @@ public static void main(String[] args) {
     Conta c1 = new Conta();
     c1.setConta(1);
     c1.setNome("Lucas");
-    c1.setSaldo(201.00);
-    c1.setLimite(200.00);
+    c1.setSaldo(500.00);
+    c1.setLimite(100.00);
 
     Conta c2 = new Conta();
     c2.setConta(2);
     c2.setNome("João");
     c2.setSaldo(45000.00);
-    c2.setLimite(1000.00);
+    c2.setLimite(200.00);
     
     System.out.println(c1.toString());
     System.out.println(c2.toString());
@@ -35,21 +35,28 @@ public static void main(String[] args) {
     System.out.println("Processando...\n");
     Double vTrans = 200d;
     
-    if ((c1.getSaldo() < c1.getLimite()) || (c2.getSaldo() < c2.getLimite())) {
-        System.out.println("Valor do limite não pode ser maior que o saldo atual da conta!");
-        return; 
-}
+    if ((vTrans <= c1.getSaldo()) && (vTrans <= c2.getSaldo())) {
     
-            if ((c2.getLimite() < vTrans) || (c1.getLimite()) < vTrans) {
-                System.out.println("Valor do limite excedido!");
-                return;
-            } else {
+            if ((c1.getSaldo() >= c1.getLimite()) && (c2.getSaldo() >= c2.getLimite())) {
+    
+                    if ((vTrans > c2.getLimite()) || (vTrans > c1.getLimite())) {
+                        System.out.println("Valor do limite excedido!");
+                        return;
+                    } else {
                 
-            Transferencia(200d, c2, c1);
-            System.out.println("Transferência concluída com sucesso!");
+                    Transferencia(200d, c2, c1);
+                    System.out.println("Transferência concluída com sucesso!");
 
-    System.out.println(c1.toString());
-    System.out.println(c2.toString());
+                    System.out.println(c1.toString());
+                    System.out.println(c2.toString());
+                     }
+          } else {
+           System.out.println("Saldo inferior ao valor do limite!");
+           return; 
+            }
+    } else {
+     System.out.println("Saldo insuficiente para efetuar transação!");
+     return; 
     }
 }       
 
